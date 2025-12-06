dae9a5faf84f5d8e34b9b24df3ec5c29667d462a
Rahul Poddar <poddarrahul58@gmail.com>
2025-12-06 18:44:19 +0530

    Todo list is done

--- Projects/Todo.py
+++ Projects/Todo.py
@@ -1,9 +1,65 @@
-def menu():
-      print("*** Menu Item ****")
-      print("1. Add a New Task")
-      print("3. View All Tasks")
-      print("4. Remove a Tasks")
-      print("5. Mark a Task Completed")
-      print("5. Exit")
+# Dictonary
+todo_list = [];
 
-      choice = input("Enter Your Choice");
+def add_task():
+      task = input("Enter a task: ")
+      todo_list.append({"Task" :task , "Status" : "pending"});
+      print("New Task Added Successfully.....\n")
+def view_task():
+      print("Your Todo List....")
+      if len(todo_list) == 0:
+            print("No pending tasks...");
+      else:
+            for index,task in enumerate(todo_list,1):
+                  print(f"{index}: {task['Task']} - {task['Status']}")
+
+def remove_task():
+      if(len(todo_list) == 0):                                   
+            print("List is Empty")
+      else: 
+            try:
+                  searchIndex = int(input("Enter the task number that you want to remove!: ")) -1;
+                  if 0<= searchIndex and searchIndex < len(todo_list):
+                        remove_task = todo_list.pop(searchIndex);
+                        print(f"Task Removed: {remove_task['Task']}")
+                  else:
+                        print("Invalid Task Number")
+            except:
+                  print("Please Enter a valid Task Number: ")
+#fucntion to create  mark done
+def mark_done():
+      try:
+            if len(todo_list) == 0:
+                  print("List is Empty...")
+            else:
+                  searchIndex = int(input("Enter the task number that you want to mark done: ")) -1;
+                  if 0 <= searchIndex and searchIndex < len(todo_list):
+                        todo_list[searchIndex]['Status'] = 'done'
+                        print(f"Task {todo_list[searchIndex]['Task']} has been marked as Done")
+                  else:
+                        print("Invalid Task Number: ")
+      except ValueError:
+            print("Please Enter a Valid Task Number")
+
+            
+
+def menu():
+      while(True):
+            print("*** Menu Item ****")
+            print("1. Add a New Task")
+            print("3. View All Tasks")
+            print("4. Remove a Tasks")
+            print("5. Mark a Task Completed")
+            print("5. Exit")
+
+            choice = input("Enter Your Choice");
+            if (choice == '1'):
+                  add_task();
+            elif(choice == '2'):
+                  view_task();
+            elif(choice == '3'):
+                  remove_task();
+            elif (choice == '4'):
+                  mark_done();
+            else:
+                  print("Invalid Choice ! Please Try Again")
